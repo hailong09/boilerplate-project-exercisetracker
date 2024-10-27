5c68037e225d107b4315415ae0ae2323f0cb5352
Hai Long Do <hldo@hais-mbp.home>
2024-10-26 21:17:43 -0400

    complete v1.0.2

--- index.js
+++ index.js
@@ -47,6 +47,61 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users/:_id/logs', async (req, res) => {
+  try {
+    const foundUser = await User.findById(req.params._id)
+    const query = { username: foundUser.username }
+
+    if (req.query.from) {
+      query['date'] = {
+        ...query['date'],
+        $gte: req.query.from
+      }
+    }
+
+    if (req.query.to) {
+      query['date'] = {
+        ...query['date'],
+        $lte: req.query.to
+      }
+    }
+    const foundExcercise = await Excercise.find(query).limit(req.query.limit)
+
+    const mapFoundExcercise = foundExcercise.map(excercise => {
+      const editedExercise = {
+        description: excercise.description,
+        duration: Number(excercise.duration),
+        date: new Date(excercise.date).toDateString()
+      }
+      return editedExercise
+    })
+    // https://3000-freecodecam-boilerplate-advond9g91z.ws-us116.gitpod.io/api/users/671b7bd83b12890779bee60b/logs?limit=1
+    return res.json(
+      {
+        username: foundUser.username,
+        count: foundExcercise.length,
+        _id: foundUser._id,
+        log: [
+          ...mapFoundExcercise
+        ]
+      }
+    )
+
+
+
+  } catch (err) {
+    console.log(err)
+  }
+})
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find().exec()
+    return res.json(users.map(user => ({ _id: user.id, username: user.username })))
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 app.post('/api/users', async (req, res) => {
   try {
     const user = new User({ username: req.body.username })
@@ -58,13 +113,10 @@ app.post('/api/users', async (req, res) => {
 })
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
-  console.log(req.body)
   try {
     const id = req.params._id
-    console.log(id)
     const foundUser = await User.findById(id).exec();
-    console.log(foundUser)
-    const date = req.body.date != '' ? new Date(req.body.date) : new Date()
+    const date = !req.body.date ? new Date() : new Date(req.body.date)
     if (foundUser) {
       const newExercise = new Excercise({
         username: foundUser.username,
